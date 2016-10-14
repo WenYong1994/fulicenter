88391497f58b0f493e3352c47b8e50e67e025d47
文勇 <779889925@qq.com>
2016-10-14 14:58:49 +0800

    实现闪频，将闪频页面设置为程序入口

--- app/src/main/java/cn/ucai/fulicenter/activity/SplashAvitivty.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/SplashAvitivty.java
@@ -1,15 +1,40 @@
 package cn.ucai.fulicenter.activity;
 
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.ucai.fulicenter.MainActivity;
 import cn.ucai.fulicenter.R;
 
 public class SplashAvitivty extends AppCompatActivity {
+
+    final long SPLASH_TIME = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_avitivty);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final Intent intent = new Intent(this,MainActivity.class);
+        new Thread(){
+            @Override
+            public void run() {
+                long startTime = SystemClock.currentThreadTimeMillis();
+                //DB 等耗时操作
+                long endTime = SystemClock.currentThreadTimeMillis();
+                if(SPLASH_TIME>(endTime-startTime)){
+                    SystemClock.sleep(SPLASH_TIME-(endTime-startTime));
+                }
+                startActivity(intent);
+            }
+        }.start();
+
     }
 }
