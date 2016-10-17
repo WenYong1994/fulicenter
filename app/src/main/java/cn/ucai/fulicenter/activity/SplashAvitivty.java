cdd0548090f63f2898828a6d1e9ae253676145c7
文勇 <779889925@qq.com>
2016-10-17 11:36:00 +0800

    优化布局文件，实现更好的适配性

--- app/src/main/java/cn/ucai/fulicenter/activity/SplashAvitivty.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/SplashAvitivty.java
@@ -1,10 +1,15 @@
 package cn.ucai.fulicenter.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.utils.MFGT;
 
@@ -16,24 +21,20 @@ public class SplashAvitivty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_avitivty);
+        ButterKnife.bind(this);
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        final Intent intent = new Intent(this,MainActivity.class);
-        new Handler().postDelayed(new Thread(){
+        final Intent intent = new Intent(this, MainActivity.class);
+        new Handler().postDelayed(new Thread() {
             @Override
             public void run() {
                 MFGT.gotoMainActivity(SplashAvitivty.this);
                 finish();
             }
-        },SPLASH_TIME);
-    }
-    @Override
-    protected void onPause() {
-        finish();
-        super.onPause();
+        }, SPLASH_TIME);
     }
 }
